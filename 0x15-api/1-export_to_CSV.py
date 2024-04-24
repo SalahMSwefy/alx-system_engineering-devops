c2f342b1e6aae69c3bbe8ee0a7e0a7a2d94b539a
SalahMSwefy <salahm.swefy@gmali.com>
2024-04-24 13:15:24 +0200

    # 0x15-api

---
+++ 0x15-api/1-export_to_CSV.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+"""
+Using what you did in the task #0,
+extend your Python script to export data in the CSV format.
+"""
+
+import requests
+import sys
+import csv
+
+if __name__ == "__main__":
+
+    user_id = sys.argv[1]
+    user = requests.get('https://jsonplaceholder.typicode.com/users/{}'.
+                        format(user_id)).json()
+    user_name = user.get('name')
+
+    tasks = requests.get('https://jsonplaceholder.typicode.com/todos?userId={}'
+                         .format(user_id)).json()
+
+    file_name = user_id + ".csv"
+    with open(file_name, mode='w') as file:
+        writer = csv.writer(file, delimiter=',', quotechar='"',
+                            quoting=csv.QUOTE_ALL, lineterminator='\n')
+        for task in tasks:
+            writer.writerow([user_id, user_name, task.get('completed'),
+                            task.get('title')])
