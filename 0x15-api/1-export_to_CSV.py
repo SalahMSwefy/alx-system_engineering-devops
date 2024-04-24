2860161eae9f6053247bb27decd48cb8957c3930
SalahMSwefy <salahm.swefy@gmali.com>
2024-04-24 13:19:13 +0200

    # 0x15-api

--- 0x15-api/1-export_to_CSV.py
+++ 0x15-api/1-export_to_CSV.py
@@ -4,9 +4,9 @@ Using what you did in the task #0,
 extend your Python script to export data in the CSV format.
 """
 
+import csv
 import requests
 import sys
-import csv
 
 if __name__ == "__main__":
 
